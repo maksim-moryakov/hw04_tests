77c9d28d591d9376af74a3719fea445aaafbab89
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-03-13 14:34:32 +0300

    правки flake8

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -47,7 +47,6 @@ class PostsPagesTests(TestCase):
                 'posts:post_detail', kwargs={'post_id': self.post.pk}),
             'posts/create_post.html': reverse(
                 'posts:post_edit', kwargs={'post_id': self.post.pk}),
-            'posts/create_post.html': reverse('posts:post_create'),
         }
         for template, reverse_name in templates_pages_names.items():
             with self.subTest(reverse_name=reverse_name):
